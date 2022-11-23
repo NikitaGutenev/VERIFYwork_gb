7a144b4d648dd7f1a5b94e3d8395cf29c08d6c15
Nikita Gutenev <gutenev229@gmail.com>
2022-11-23 17:29:40 +0300

    завершил работу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] arr = {"hello","bye","Steve","Mia",":("};
+int count = 0;
+String[] array = new string[arr.Length];
+for (int i = 0; i<arr.Length; i++)
+{
+    if (arr[i].Length <= 3) 
+    {
+        array[count] = arr[i];
+        count++;
+    }
+}
+for (int i = 0; i<array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
